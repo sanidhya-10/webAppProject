9b6004c4ab06dddfda4a7ac1edd5ba7530e39286
sanidhya-10 <79214462+sanidhya-10@users.noreply.github.com>
2021-02-17 22:04:49 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -6,8 +6,8 @@ var mysql = require("mysql");
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "password",
+  user: "***",
+  password: "***",
   database: "mydb",
 });
 
